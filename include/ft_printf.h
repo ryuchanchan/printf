5f0913ddf552f98e8e97f28732173a41416db293
kawamuraryuu <kawamuraryuu@kawamuryuunoair.(none)>
2022-06-13 09:20:34 +0900

    norm c

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -19,15 +19,15 @@
 #include <stdlib.h>
 
 int put_nbr_u(unsigned long long n);
-char    *point_base(unsigned long long p, int base);
 int str_counter(char *c);
 int	ft_putchar(char c);
 int ft_putstr(char *str);
-char	*lower_case(char *c);
 int put_s(char *s);
 int put_d_i(int i);
 int put_p(unsigned long long p);
 int put_u(unsigned long long u);
 int put_xX(unsigned int i, int f);
 int ft_printf(const char    *format, ...);
+char    *point_base(unsigned long long p, int base);
+char	*lower_case(char *c);
 #endif
